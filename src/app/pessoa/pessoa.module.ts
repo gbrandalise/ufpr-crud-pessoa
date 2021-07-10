805fcf1f3f168388b7c07f41471d42c9e6dd87d9
giovannybrandalise <giovannybrandalise@brq.com>
2021-07-10 16:20:38 -0300

    fontawesome modulo pessoa servico pessoa componente listar pessoa

---
+++ src/app/pessoa/pessoa.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PessoaService } from './services/pessoa.service';
+import { ListarPessoaComponent } from './listar-pessoa/listar-pessoa.component';
+
+@NgModule({
+    declarations: [
+    ListarPessoaComponent
+  ],
+    imports: [
+        CommonModule
+    ],
+    providers: [
+        PessoaService
+    ]
+})
+export class PessoaModule { }
